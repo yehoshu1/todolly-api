82b3339f5892f0134fff7e165299c2434b6a0b91
Joshua <joshuashalla1@gmail.com>
2025-08-13 22:53:25 +0200

    Setup database schema

---
+++ src/database/index.ts
@@ -0,0 +1,6 @@
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/libsql';
+
+const db = drizzle(process.env.DB_FILE_NAME!);
+
+export default db;
---
+++ src/database/schema.ts
@@ -0,0 +1,47 @@
+import { sql } from "drizzle-orm";
+import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const userTable = sqliteTable("users", {
+    id: int("id").primaryKey().unique(),
+    email: text("email").unique().notNull(),
+    name: text("name").notNull(),
+    password: text("password").notNull(),
+});
+
+export const tasks = sqliteTable("tasks", {
+  taskId: int("task_id").primaryKey().unique(),
+  title: text("title").notNull(),
+  description: text("description"),
+  dueDate: text("due_date"), // Store DATE as ISO string (YYYY-MM-DD)
+  dueTime: text("due_time"), // Store TIME as HH:MM:SS
+  location: text("location"),
+  status: text("status"),
+  priority: text("priority"),
+  createdAt: text("created_at").default(sql`CURRENT_TIMESTAMP`).notNull(),
+  updatedAt: text("updated_at").default(sql`CURRENT_TIMESTAMP`).notNull(),
+});
+
+
+export const subtasks = sqliteTable("subtasks", {
+  subtaskId: int("subtask_id").primaryKey().unique(),
+  taskId: int("task_id")
+    .notNull()
+    .references(() => tasks.taskId, { onDelete: "cascade" }),
+  title: text("title").notNull(),
+  description: text("description"),
+  status: text("status"),
+  priority: text("priority"),
+  createdAt: text("created_at").default(sql`CURRENT_TIMESTAMP`).notNull(),
+  updatedAt: text("updated_at").default(sql`CURRENT_TIMESTAMP`).notNull(),
+});
+
+
+export const reminders = sqliteTable("reminders", {
+  reminderId: int("reminder_id").primaryKey().unique(),
+  taskId: int("task_id")
+    .notNull()
+    .references(() => tasks.taskId, { onDelete: "cascade" }),
+  remindAt: text("remind_at").notNull(), // Store DATETIME as ISO 8601 string
+  message: text("message"),
+});
+
